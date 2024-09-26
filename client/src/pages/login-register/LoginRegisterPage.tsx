cca37f42475e6c3e8fa6d0c7d90146a4aaa40b1d
marquescauee <cauelopesmarques123@gmail.com>
2024-09-26 10:58:59 -0300

    feat: added toast to login flow

--- client/src/pages/login-register/LoginRegisterPage.tsx
+++ client/src/pages/login-register/LoginRegisterPage.tsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react'
 import Login from '../../components/login/Login'
 import Register from '../../components/register/Register'
 import { AuthProvider } from '../../contexts/useAuth'
 import './LoginRegisterPage.css'
 
 const LoginRegisterPage = () => {
+  const [hasRoadmapToSave, setHasRoadmapToSave] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('roadmapToBeSaved')) {
+      setHasRoadmapToSave(true)
+    }
+  }, [])
+
   return (
-    <div className="login-register-page">
-      <AuthProvider>
-        <Login />
-        <Register />
-      </AuthProvider>
-    </div>
+    <>
+      <div className="login-register-page">
+        <AuthProvider>
+          <Login />
+          <Register />
+        </AuthProvider>
+      </div>
+      {hasRoadmapToSave && (
+        <div className="toast login-to-save-roadmap">
+          Por favor, realize o login para salvar seu roadmap!
+        </div>
+      )}
+    </>
   )
 }
 
